3b2d9e23fd93abd4a412fe8b5a7c531f5e8da6fc
Mehmet Baykar <mbaykar@userlytics.com>
2020-11-24 15:48:07 +0300

    Defines swift version to 5.

--- Package.swift
+++ Package.swift
@@ -15,6 +15,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
+    
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
@@ -24,5 +25,7 @@ let package = Package(
         .testTarget(
             name: "TouchVisualizerTests",
             dependencies: ["TouchVisualizer"]),
-    ]
+    ],
+    
+    swiftLanguageVersions: [.v5]
 )
